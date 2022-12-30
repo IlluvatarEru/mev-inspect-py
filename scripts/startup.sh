5c677cc9a4d1df9b0cfc66221512f108ae456b48
Eru Ilúvatar <arthurbdauphine@gmail.com>
2022-12-30 07:43:10 +0000

    Update scripts/startup.sh
    Co-authored-by: Luca G.F. <luca.georges-francois@epitech.eu>

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -4,7 +4,7 @@
 # this assumes you already ran: kind create cluster
 # Do not forget to add your RPC URL below
 export RPC_URL=""
-tilt down;
-tilt up;
-./mev exec alembic upgrade head;
-./mev prices fetch-all;
+tilt down &&
+tilt up &&
+./mev exec alembic upgrade head &&
+./mev prices fetch-all
