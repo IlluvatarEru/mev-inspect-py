ee990abb28bfa965bde0c9011570b098d808e4f1
Eru Ilúvatar <arthurbdauphine@gmail.com>
2023-01-13 10:39:59 +0100

    feat: add a startup script - closes #12 (#18)
    * feat: add a startup script - closes #12
    
    Signed-off-by: Arthurim <arthurbdauphine@gmail.com>
    
    * fix: add doc about the startup script
    
    Signed-off-by: Arthurim <arthurbdauphine@gmail.com>
    
    * Update scripts/startup.sh
    
    Co-authored-by: Luca G.F. <luca.georges-francois@epitech.eu>
    
    Signed-off-by: Arthurim <arthurbdauphine@gmail.com>
    Co-authored-by: Luca G.F. <luca.georges-francois@epitech.eu>

--- scripts/commit.sh
+++ scripts/commit.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # bash script to commit in accordance with the branch name
-# usage: ./scripts/commit.sh
+# usage: bash scripts/commit.sh
 branch=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
 branch="${branch##*feat/}"
 branch="${branch//\_/ }"
