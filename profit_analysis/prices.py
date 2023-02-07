4f9e85748dacad10ecd22dffc7c361a0bd4a3214
Arthurim <arthurbdauphine@gmail.com>
2023-02-07 16:05:32 +0000

    feat: histogram - closes #fix/fix
    Signed-off-by: Arthurim <arthurbdauphine@gmail.com>

--- profit_analysis/prices.py
+++ profit_analysis/prices.py
@@ -4,10 +4,10 @@ from time import sleep
 from typing import Union
 
 import pandas as pd
-
-from mev_inspect.web3_provider import W3
 from profit_analysis.chains import ETHEREUM_CHAIN, POLYGON_CHAIN
 from profit_analysis.column_names import BLOCK_KEY, PRICE_KEY
+
+from mev_inspect.web3_provider import W3
 
 UNISWAP_V2_PAIR_ABI = json.loads(
     '[{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"owner","type":"address"},{"indexed":true,"internalType":"address","name":"spender","type":"address"},{"indexed":false,"internalType":"uint256","name":"value","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"sender","type":"address"},{"indexed":false,"internalType":"uint256","name":"amount0","type":"uint256"},{"indexed":false,"internalType":"uint256","name":"amount1","type":"uint256"},{"indexed":true,"internalType":"address","name":"to","type":"address"}],"name":"Burn","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"sender","type":"address"},{"indexed":false,"internalType":"uint256","name":"amount0","type":"uint256"},{"indexed":false,"internalType":"uint256","name":"amount1","type":"uint256"}],"name":"Mint","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"sender","type":"address"},{"indexed":false,"internalType":"uint256","name":"amount0In","type":"uint256"},{"indexed":false,"internalType":"uint256","name":"amount1In","type":"uint256"},{"indexed":false,"internalType":"uint256","name":"amount0Out","type":"uint256"},{"indexed":false,"internalType":"uint256","name":"amount1Out","type":"uint256"},{"indexed":true,"internalType":"address","name":"to","type":"address"}],"name":"Swap","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"internalType":"uint112","name":"reserve0","type":"uint112"},{"indexed":false,"internalType":"uint112","name":"reserve1","type":"uint112"}],"name":"Sync","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"from","type":"address"},{"indexed":true,"internalType":"address","name":"to","type":"address"},{"indexed":false,"internalType":"uint256","name":"value","type":"uint256"}],"name":"Transfer","type":"event"},{"constant":true,"inputs":[],"name":"DOMAIN_SEPARATOR","outputs":[{"internalType":"bytes32","name":"","type":"bytes32"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"MINIMUM_LIQUIDITY","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"PERMIT_TYPEHASH","outputs":[{"internalType":"bytes32","name":"","type":"bytes32"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"internalType":"address","name":"","type":"address"},{"internalType":"address","name":"","type":"address"}],"name":"allowance","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"spender","type":"address"},{"internalType":"uint256","name":"value","type":"uint256"}],"name":"approve","outputs":[{"internalType":"bool","name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"internalType":"address","name":"","type":"address"}],"name":"balanceOf","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"to","type":"address"}],"name":"burn","outputs":[{"internalType":"uint256","name":"amount0","type":"uint256"},{"internalType":"uint256","name":"amount1","type":"uint256"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"decimals","outputs":[{"internalType":"uint8","name":"","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"factory","outputs":[{"internalType":"address","name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"getReserves","outputs":[{"internalType":"uint112","name":"_reserve0","type":"uint112"},{"internalType":"uint112","name":"_reserve1","type":"uint112"},{"internalType":"uint32","name":"_blockTimestampLast","type":"uint32"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"_token0","type":"address"},{"internalType":"address","name":"_token1","type":"address"}],"name":"initialize","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"kLast","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"to","type":"address"}],"name":"mint","outputs":[{"internalType":"uint256","name":"liquidity","type":"uint256"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"name","outputs":[{"internalType":"string","name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"internalType":"address","name":"","type":"address"}],"name":"nonces","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"owner","type":"address"},{"internalType":"address","name":"spender","type":"address"},{"internalType":"uint256","name":"value","type":"uint256"},{"internalType":"uint256","name":"deadline","type":"uint256"},{"internalType":"uint8","name":"v","type":"uint8"},{"internalType":"bytes32","name":"r","type":"bytes32"},{"internalType":"bytes32","name":"s","type":"bytes32"}],"name":"permit","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"price0CumulativeLast","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"price1CumulativeLast","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"to","type":"address"}],"name":"skim","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"internalType":"uint256","name":"amount0Out","type":"uint256"},{"internalType":"uint256","name":"amount1Out","type":"uint256"},{"internalType":"address","name":"to","type":"address"},{"internalType":"bytes","name":"data","type":"bytes"}],"name":"swap","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"symbol","outputs":[{"internalType":"string","name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"sync","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"token0","outputs":[{"internalType":"address","name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"token1","outputs":[{"internalType":"address","name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"totalSupply","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"value","type":"uint256"}],"name":"transfer","outputs":[{"internalType":"bool","name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"value","type":"uint256"}],"name":"transferFrom","outputs":[{"internalType":"bool","name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"}]'
@@ -54,7 +54,7 @@ class UniswapPricer:
         self.block_to_price = {}
 
     async def get_decimals_from_token(self, token):
-        contract = self.w3_provider.w3_provider_async.eth.contract(
+        contract = self.w3_provider.w3_provider_archival.eth.contract(
             address=token, abi=ERC20_ABI
         )
         decimals = await contract.functions.decimals().call()
@@ -68,22 +68,24 @@ class UniswapPricer:
             try:
                 self._token_target_address = token_target_address
                 if token_target_address != self._token_base_address:
-                    factory = self.w3_provider.w3_provider_async.eth.contract(
+                    factory = self.w3_provider.w3_provider_archival.eth.contract(
                         address=self._factory, abi=UNISWAP_V2_FACTORY_ABI
                     )
                     pair_address = await factory.functions.getPair(
                         self._token_base_address, token_target_address
                     ).call()
-                    pair_contract = self.w3_provider.w3_provider_async.eth.contract(
+
+                    pair_contract = self.w3_provider.w3_provider_archival.eth.contract(
                         address=pair_address, abi=UNISWAP_V2_PAIR_ABI
                     )
+
                     self._pair = pair_contract
                     self._token_base_decimals = (
-                            10
-                            ** await self.get_decimals_from_token(self._token_base_address)
+                        10
+                        ** await self.get_decimals_from_token(self._token_base_address)
                     )
                     self._token_target_decimals = (
-                            10 ** await self.get_decimals_from_token(token_target_address)
+                        10 ** await self.get_decimals_from_token(token_target_address)
                     )
                     token_n = await self.is_target_token0_or_token1()
                     self._is_target_token0_or_token1 = token_n
@@ -125,9 +127,9 @@ class UniswapPricer:
                             token_target_reserves = reserves[1]
                             token_base_reserves = reserves[0]
                         price = (
-                                (float(token_base_reserves) / float(token_target_reserves))
-                                * self._token_target_decimals
-                                / self._token_base_decimals
+                            (float(token_base_reserves) / float(token_target_reserves))
+                            * self._token_target_decimals
+                            / self._token_base_decimals
                         )
 
                     price = float(price)
@@ -151,11 +153,11 @@ async def safe_get_price(pricer, block, max_concurrency_semaphore):
 
 
 async def get_uniswap_historical_prices(
-        target_blocks,
-        token_address,
-        chain=POLYGON_CHAIN,
-        max_concurrency=10,
-        block_batch_size=1024,
+    target_blocks,
+    token_address,
+    chain=POLYGON_CHAIN,
+    max_concurrency=10,
+    block_batch_size=1024,
 ):
     target_blocks = [int(b) for b in target_blocks]
     print(f"Requesting prices for {token_address} for blocks={target_blocks}")
@@ -179,16 +181,21 @@ async def get_uniswap_historical_prices(
                 )
         await asyncio.gather(*tasks)
         block_to_price = pricer.block_to_price
-        prices = pd.DataFrame(list(block_to_price.items()), columns=[BLOCK_KEY, PRICE_KEY])
+        prices = pd.DataFrame(
+            list(block_to_price.items()), columns=[BLOCK_KEY, PRICE_KEY]
+        )
         prices = prices.loc[prices[PRICE_KEY] > 0]
-        print(f'prices={prices}')
+        print(f"prices={prices}")
         return prices
     else:
         # get prices for 10 blocks on each side in case the nodes are out of sync for the target block
         target_block = int(target_blocks[0])
         n_blocks_on_each_side = 10
-        target_blocks = [target_block - i for i in range(n_blocks_on_each_side)] + [target_block] + \
-                        [target_block + i for i in range(n_blocks_on_each_side)]
+        target_blocks = (
+            [target_block - i for i in range(n_blocks_on_each_side)]
+            + [target_block]
+            + [target_block + i for i in range(n_blocks_on_each_side)]
+        )
         return await get_uniswap_historical_prices(
             target_blocks,
             token_address,
